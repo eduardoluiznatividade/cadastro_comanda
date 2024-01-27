d5919121b334a929ae3e08c05e0551f168820fdd
Natividade <Natividade@eln>
2024-01-27 18:52:19 -0300

    Inicio Projeto

---
+++ cadastro-comanda/src/main/java/br/edu/infnet/cadastrocomanda/CadastroComandaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.cadastrocomanda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroComandaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroComandaApplication.class, args);
+	}
+
+}
---
+++ cadastro-comanda/src/test/java/br/edu/infnet/cadastrocomanda/CadastroComandaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.cadastrocomanda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroComandaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
